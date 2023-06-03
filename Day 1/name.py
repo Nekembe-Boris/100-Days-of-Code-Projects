4473921bb6e6887def9bd438cfb75fc176f12330
nekembe <nekembeb@gmail.com>
2023-06-03 13:18:33 +0100

    Change city prompt question

--- Day 1/name.py
+++ Day 1/name.py
@@ -1,6 +1,6 @@
 print("Welcome to the Band Name Generator\n")
 
-city_name = input("What is the name of your city? \n")
+city_name = input("Which city did you grow up in? \n")
 pet_name = input("What is the name of your pet? \n")
 
 print("Then your Band name could be " + city_name +" "+ pet_name)
