937b36badabe9425887546bf022d7075d3f0ddf8
nekembe <nekembeb@gmail.com>
2023-06-03 13:10:16 +0100

    This is Day 1 project

--- Day 1/name.py
+++ Day 1/name.py
@@ -1,1 +1,6 @@
+print("Welcome to the Band Name Generator\n")
 
+city_name = input("What is the name of your city? \n")
+pet_name = input("What is the name of your pet? \n")
+
+print("Then your Band name could be " + city_name +" "+ pet_name)
