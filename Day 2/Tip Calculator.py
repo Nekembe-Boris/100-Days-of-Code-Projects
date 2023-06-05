86fdc68fd4048144840e02ce5ad31162475daba9
nekembe <nekembeb@gmail.com>
2023-06-05 02:01:50 +0100

    Adding the dollar sign and rearranging the order of the code

--- Day 2/Tip Calculator.py
+++ Day 2/Tip Calculator.py
@@ -1,12 +1,12 @@
 print("Welcome to the Tip Calculator \n")
 
-bill = float(input("What was the total bill? \n"))
+bill = float(input("What was the total bill ? \n$"))
 
 tip_category = int(input("What percentage tip will you like to give? 10, 12, or 15?  \n"))
 
-tip_percentage = tip_category / 100
-
 split = int(input("How many people will you like to split the bill? \n"))
+
+tip_percentage = tip_category / 100
 
 per_person = ((bill * tip_percentage) + bill) / split
 
