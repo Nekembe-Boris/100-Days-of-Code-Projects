81f5494c84a4d9b22157a9dde113fe167f8363cf
Nekembe-Boris <82305521+Nekembe-Boris@users.noreply.github.com>
2023-06-05 01:39:55 +0100

    The first version of the Tip calculator

---
+++ Day 2/Tip Calculator.py
@@ -0,0 +1,13 @@
+print("Welcome to the Tip Calculator \n")
+
+bill = float(input("What was the total bill? \n"))
+
+tip_category = int(input("What percentage tip will you like to give? 10, 12, or 15?  \n"))
+
+tip_percentage = tip_category / 100
+
+split = int(input("How many people will you like to split the bill? \n"))
+
+per_person = ((bill * tip_percentage) + bill) / split
+
+print(f"Each person should pay: {round(per_person, 2)}")
