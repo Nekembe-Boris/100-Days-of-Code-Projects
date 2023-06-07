2b525854f75a55f68a2cb9c5bad5fdc3bb9d8f22
nekembe <nekembeb@gmail.com>
2023-06-07 15:04:31 +0100

    readjusting vocabulary errors

--- Day 3/Treasure_Island.py
+++ Day 3/Treasure_Island.py
@@ -1,30 +1,44 @@
 print('''
-      ~;
-                  ,/|\,
-                ,/' |\ \,
-              ,/'   | |  \
-            ,/'     | |   |
-          ,/'       |/    |
-        ,/__________|-----' ,
-       ___.....-----''-----/
- jgs   \                  /
-   ~~-~^~^~`~^~`~^^~^~-^~^~^~-~^~^
-     ~-^~^-`~^~-^~^`^~^-^~^`^~^-~^
+       ____________________________________________________________________
+ / \-----     ---------  -----------     -------------- ------    ----\
+ \_/__________________________________________________________________/
+ |~ ~~ ~~~ ~ ~ ~~~ ~ _____.----------._ ~~~  ~~~~ ~~   ~~  ~~~~~ ~~~~|
+ |  _   ~~ ~~ __,---'_       "         `. ~~~ _,--.  ~~~~ __,---.  ~~|
+ | | \___ ~~ /      ( )   "          "   `-.,' (') \~~ ~ (  / _\ \~~ |
+ |  \    \__/_   __(( _)_      (    "   "     (_\_) \___~ `-.___,'  ~|
+ |~~ \     (  )_(__)_|( ))  "   ))          "   |    "  \ ~~ ~~~ _ ~~|
+ |  ~ \__ (( _( (  ))  ) _)    ((     \\//    " |   "    \_____,' | ~|
+ |~~ ~   \  ( ))(_)(_)_)|  "    ))    //\\ " __,---._  "  "   "  /~~~|
+ |    ~~~ |(_ _)| | |   |   "  (   "      ,-'~~~ ~~~ `-.   ___  /~ ~ |
+ | ~~     |  |  |   |   _,--- ,--. _  "  (~~  ~~~~  ~~~ ) /___\ \~~ ~|
+ |  ~ ~~ /   |      _,----._,'`--'\.`-._  `._~~_~__~_,-'  |H__|  \ ~~|
+ |~~    / "     _,-' / `\ ,' / _'  \`.---.._          __        " \~ |
+ | ~~~ / /   .-' , / ' _,'_  -  _ '- _`._ `.`-._    _/- `--.   " " \~|
+ |  ~ / / _-- `---,~.-' __   --  _,---.  `-._   _,-'- / ` \ \_   " |~|
+ | ~ | | -- _    /~/  `-_- _  _,' '  \ \_`-._,-'  / --   \  - \_   / |
+ |~~ | \ -      /~~| "     ,-'_ /-  `_ ._`._`-...._____...._,--'  /~~|
+ | ~~\  \_ /   /~~/    ___  `---  ---  - - ' ,--.     ___        |~ ~|
+ |~   \      ,'~~|  " (o o)   "         " " |~~~ \_,-' ~ `.     ,'~~ |
+ | ~~ ~|__,-'~~~~~\    \"/      "  "   "    /~ ~~   O ~ ~~`-.__/~ ~~~|
+ |~~~ ~~~  ~~~~~~~~`.______________________/ ~~~    |   ~~~ ~~ ~ ~~~~|
+ |____~jrei~__~_______~~_~____~~_____~~___~_~~___~\_|_/ ~_____~___~__|
+ / \----- ----- ------------  ------- ----- -------  --------  -------\
+ \_/__________________________________________________________________/
 ''')
 
-print("Welcome to Treasure Island.")
+print("Welcome to Treasure Hunt!!.")
 print("Your mission is to find the treasure.")
 
 junction = input("You're at a confluence which direction do you want to take? Right or Left? \n").lower()
 
 if junction == "left":
-    step1 = input("Do you want to Wait for the yatch or Swim ? \n").lower()
+    step1 = input("Do you want to Wait for the yatch or swim ? \n").lower()
     if step1 == "wait":
-        step2 = input("You are now at the Island. Which door do you want to open? Red, Blue or Yellow \n").lower()
+        step2 = input("You arrived at the Island unharmed. There is a house with 3 doors. Which door do you want to open? Red, Blue or Yellow? \n").lower()
         if step2 == "yellow":
             print("You found the treasure! You Win!")
         elif step2 == "blue":
-            print("You enter a room of beasts. GAME OVER")
+            print("You entered a room of beasts. GAME OVER")
         elif step2 == "red":
             print("You chose a door that doesn't exist. GAME OVER")
         else:
