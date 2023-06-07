74edf3ccdceb46559b41e560affd5c8dff4e0c4a
nekembe <nekembeb@gmail.com>
2023-06-07 14:50:09 +0100

    This makes the code more redundant

--- Day 3/Treasure_Island.py
+++ Day 3/Treasure_Island.py
@@ -15,20 +15,18 @@ print('''
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 
-junction = input("You're at a confluence which direction do you want to take? Right or Left? \n")
-lower_junction = junction.lower()
+junction = input("You're at a confluence which direction do you want to take? Right or Left? \n").lower()
 
-if lower_junction == "left":
-    step1 = input("Do you want to Wait for the yatch or Swim ? \n")
-    lower_step1 = step1.lower()
-    if lower_step1 == "wait":
-        step2 = input("You are now at the Island. Which door do you want to open? Red, Blue or Yellow \n")
-        lower_step2 = step2.lower()
-        if lower_step2 == "yellow":
+
+if junction == "left":
+    step1 = input("Do you want to Wait for the yatch or Swim ? \n").lower()
+    if step1 == "wait":
+        step2 = input("You are now at the Island. Which door do you want to open? Red, Blue or Yellow \n").lower()
+        if step2 == "yellow":
             print("You found the treasure! You Win!")
-        elif lower_step2 == "blue":
+        elif step2 == "blue":
             print("You enter a room of beasts. GAME OVER")
-        elif lower_step2 == "red":
+        elif step2 == "red":
             print("You chose a door that doesn't exist. GAME OVER")
         else:
             print("You fell into a hole. GAME OVER")
