1e2b5b1c142bfff0c2caa1918f734984aa735b94
nekembe <nekembeb@gmail.com>
2023-06-09 20:10:56 +0100

    Rock Paper Scissors (RPS) is a zero-sum game, typically played by two people using their hands and no tools.

---
+++ Day 4/Rock Paper Scissors.py
@@ -0,0 +1,76 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+choice = int(input("\nWhat do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \n"))
+print(type(choice))
+
+sign = "Any"
+
+if choice == 0:
+    sign = rock
+elif choice == 1:
+    sign = paper
+else:
+    sign = scissors
+
+print(sign)
+
+
+random_int = random.randint(0, 2)
+print(random_int)
+
+random_sign = "Any"
+
+if random_int == 0:
+    random_sign = rock
+elif random_int == 1:
+    random_sign = paper
+else:
+    random_sign = scissors
+
+print(random_sign)
+
+if sign == random_sign:
+    print(f"You chose\n {sign})")
+    print(f"The computer chose\n {random_sign}")
+    print("There is a draw!")
+elif (sign == rock) and (random_sign == scissors):
+    print(f"You chose\n {sign})")
+    print(f"The computer chose\n {random_sign}")
+    print("You won!")
+elif (sign == scissors) and (random_sign == paper):
+    print(f"You chose\n {sign})")
+    print(f"The computer chose\n {random_sign}")
+    print("You won!")
+elif (sign == paper) and (random_sign == rock):
+    print(f"You chose\n {sign})")
+    print(f"The computer chose\n {random_sign}")
+    print("You won!")
+else:
+    print("You lose")
