19d08f155277d3041df27eaa43e02bfff99ef94f
nekembe <nekembeb@gmail.com>
2023-06-17 16:05:34 +0100

    verifying if player guess is correct, tracking the lives left, checking for duplicate guesses, and verifying if player won or not

--- Day 7/Hangman.py
+++ Day 7/Hangman.py
@@ -306,3 +306,37 @@ print(logo)
 print("Welcome to HANGMAN!!")
 
 print(f"\nWORD: {placeholder}")
+
+while end_game != True:
+
+    #getting user input and inserting it in a list if it matches any letter
+    user_guess = input("\nGUESS A LETTER IN THE WORD:\n").lower()
+
+    #checking if guessed letter has already been chosen else, including it in list
+    if user_guess in display:
+        print("You've already chosen this letter. Try again")
+    else:
+        for i in range(len(random_word)):
+            if random_word[i] == user_guess:
+                display[i] = user_guess
+
+    #forming a placeholder for all the corret guesses and printing them out
+    main_placeholder = ""
+    for n in range(0, len(display)):
+        all_dash = display[n]
+        main_placeholder = main_placeholder + all_dash
+    print(f"\nWORD: {main_placeholder}")
+
+    #verifying if player guessed the wrong word and tracking lives left
+    if user_guess not in random_word:
+        lives_left -= 1
+        print(f"You guessed letter {user_guess} and it's not in word")
+        print(stages[lives_left])
+    
+    #ending game if player won or lost all lives
+    if "_" not in display:
+        end_game = True
+        print("You Won!")
+    elif lives_left == 0:
+        end_game = True
+        print("You Lost")       
