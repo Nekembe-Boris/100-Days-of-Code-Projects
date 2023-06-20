ee605a2ef030f30da8903bc3c3b7990b833b8571
nekembe <nekembeb@gmail.com>
2023-06-20 15:01:10 +0100

    Decode model

--- Day 8/ceasar-cipher.py
+++ Day 8/ceasar-cipher.py
@@ -1,45 +1,56 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def ceasar(character, code):
-  cipher = 0
-  new_char = ""
-  for i in range(len(character)):
-    letter = character[i]
-    current_letter = letter
+def ceasar(action, character, code):
+  
+  if action == "encode":
+    cipher = 0
+    new_message = ""
+    for i in range(len(character)):
+      letter = character[i]
+      current_letter = letter
 
-    for item_number in range(len(alphabet)):
+      for letter_number in range(len(alphabet)):
 
-      if current_letter == alphabet[item_number]:
-        cipher = item_number + code
+        if current_letter == alphabet[letter_number]:
+          cipher = letter_number + code
 
-        if cipher > item_number:
-          excess = cipher - len(alphabet)
-          crypted = alphabet[excess]
-          new_char += crypted
-        else:
-          crypted = alphabet[cipher]
-          new_char += crypted      
-        # if cipher > n:
-        #    print(n)
+          if cipher > letter_number:
+            excess = cipher - len(alphabet)
+            crypted = alphabet[excess]
+            new_message += crypted
+          else:
+            crypted = alphabet[cipher]
+            new_message += crypted      
+  
+    print(new_message)
+
+  elif action == "decode":
+    cipher = 0
+    original_message = ""
+    for i in range(len(character)):
+      de_letter = character[i]
+      key_letter = de_letter
+
+      for letter_number in range(len(alphabet)):
+
+        if key_letter == alphabet[letter_number]:
+          cipher = letter_number - code
+          original_message += alphabet[cipher]
+            
+    print(original_message)
         
-        
-  print(new_char)
+  
 
-ceasar(text, shift)
+ceasar(direction, text, shift)
 
-# cipher = 0
+cipher = 0
+
 
 # for i in range(len(alphabet)):
 #     if text == alphabet[i]:
-#       cipher = i + shift
-#     if cipher > i:
-#         excess = cipher - len(alphabet)
-#         crypted = alphabet[excess]
-#     else:
-#         crypted = alphabet[cipher]
-
-# print(crypted)
+#       cipher = i - shift
+#       crypted = alphabet[cipher]
