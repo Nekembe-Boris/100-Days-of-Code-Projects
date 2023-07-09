f2b45e63d3c61cc10a628675d8afbdc7173908c5
nekembe <nekembeb@gmail.com>
2023-07-09 18:33:45 +0100

    first commit

---
+++ Day16/main.py
@@ -0,0 +1,31 @@
+from menu_item import Menu, MenuItem
+from coffeemaker_class import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+
+end_game = False
+
+while end_game != True:
+
+    available = menu.get_items()
+
+    request = input(f"What would you like? {available}: ").lower()
+
+    if request == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    elif request == "off": 
+        print("Switching off....")
+        end_game = True
+    else:
+        my_choice = menu.find_drink(request)
+        if my_choice == None:
+            print("Valid order needed!")
+        elif coffee_maker.is_resource_sufficient(my_choice) == True and money_machine.make_payment(my_choice.cost) == True:
+            coffee_maker.make_coffee(my_choice)
+        
