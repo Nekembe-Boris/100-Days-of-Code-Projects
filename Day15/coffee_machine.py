7cf225f26dd692dab942d82e339ff0f96c84c7c6
nekembe <nekembeb@gmail.com>
2023-07-09 19:01:33 +0100

    adding flexibility

--- Day15/coffee_machine.py
+++ Day15/coffee_machine.py
@@ -112,7 +112,7 @@ def coffee():
                         resources[items] -= order_materials[items]
                 if total_sum > order_price:
                     print(f"Here is ${balance} in change")
-                print("Here is your coffee ☕, Enjoy!")        
+                print(f"Here is your {request.title()} ☕, Enjoy!")        
         else:
             print("Please, enter a valid order")
 
