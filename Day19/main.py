ed2071f499ee5d2c9d9d50c56bc6363c64d6f9be
nekembe <nekembeb@gmail.com>
2023-07-14 12:22:55 +0100

    initiating object race

--- Day19/main.py
+++ Day19/main.py
@@ -27,3 +27,21 @@ screen.setup(height=600, width=700)
 user_bet = screen.textinput("Place a Bet", "Which turtle will win the race. Enter a color: ").lower()
 
 end_race = False
+
+while end_race != True:
+
+    for i in turtle_list:
+        i.forward(random.randint(0, 4))
+        position = round(i.xcor())
+
+
+        if position > 315:
+            end_race = True
+            if user_bet == i.color()[0]:
+                print(f"You won. The winner is the {i.color()[0]} turtle")
+            else:
+                print(f"You lose. The winner is the {i.color()[0]} turtle")
+            
+
+
+screen.exitonclick()
