4900e5fce2e7dc1712fa258797fec29978c20fd0
nekembe <nekembeb@gmail.com>
2023-07-25 15:54:38 +0100

    recreating letters

--- Day24/mail_merge/main.py
+++ Day24/mail_merge/main.py
@@ -2,9 +2,9 @@ with open("100-Days-of-Code-Projects\Day24\mail_merge\Input\Letters\starting_let
     letter_body = data.read()
     print(letter_body)
 
-with open(r"100-Days-of-Code-Projects\Day24\mail_merge\Input\Names\invited_names.txt") as names:
-    for lines in names:
-        invited = lines.rstrip()
-        invited_person = letter_body.replace('name', invited)
+with open(r"100-Days-of-Code-Projects\Day24\mail_merge\Input\Names\invited_names.txt") as names_list:
+    for name in names_list:
+        invited = name.rstrip()
+        invited_person = letter_body.replace('[name]', invited)
         with open(rf"100-Days-of-Code-Projects\Day24\mail_merge\Output\ReadyToSend\{invited}.txt", mode="w") as final_letter:
             final_letter.write(invited_person)
