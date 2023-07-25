3655365e68ecfd4c374e9fc8f95385f9a019a732
nekembe <nekembeb@gmail.com>
2023-07-25 15:49:15 +0100

    removing comments

--- Day24/mail_merge/main.py
+++ Day24/mail_merge/main.py
@@ -8,13 +8,3 @@ with open(r"100-Days-of-Code-Projects\Day24\mail_merge\Input\Names\invited_names
         invited_person = letter_body.replace('name', invited)
         with open(rf"100-Days-of-Code-Projects\Day24\mail_merge\Output\ReadyToSend\{invited}.txt", mode="w") as final_letter:
             final_letter.write(invited_person)
-
-
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
