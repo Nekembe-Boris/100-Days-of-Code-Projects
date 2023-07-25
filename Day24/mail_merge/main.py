d396dabf25d3ffb4a844256b570cf829bfc47f5d
nekembe <nekembeb@gmail.com>
2023-07-25 19:27:11 +0100

    rearranging file path

--- Day24/mail_merge/main.py
+++ Day24/mail_merge/main.py
@@ -1,9 +1,9 @@
-with open("100-Days-of-Code-Projects\Day24\mail_merge\Input\Letters\starting_letter.txt") as data:
+with open(".\Input\Letters\starting_letter.txt") as data:
     letter_body = data.read()
 
-with open(r"100-Days-of-Code-Projects\Day24\mail_merge\Input\Names\invited_names.txt") as names_list:
+with open(r".\Input\Names\invited_names.txt") as names_list:
     for name in names_list:
         invited_person = name.rstrip()
         invitation_letter = letter_body.replace('[name]', invited_person)
-        with open(rf"100-Days-of-Code-Projects\Day24\mail_merge\Output\ReadyToSend\{invited_person}.txt", mode="w") as final_letter:
+        with open(rf".\Output\ReadyToSend\{invited_person}.txt", mode="w") as final_letter:
             final_letter.write(invitation_letter)
