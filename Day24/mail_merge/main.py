8ca936a6c113bb7302f67b8d91cc0d15fc11cbcc
nekembe <nekembeb@gmail.com>
2023-07-25 15:59:55 +0100

    making code more readable

--- Day24/mail_merge/main.py
+++ Day24/mail_merge/main.py
@@ -1,10 +1,9 @@
 with open("100-Days-of-Code-Projects\Day24\mail_merge\Input\Letters\starting_letter.txt") as data:
     letter_body = data.read()
-    print(letter_body)
 
 with open(r"100-Days-of-Code-Projects\Day24\mail_merge\Input\Names\invited_names.txt") as names_list:
     for name in names_list:
-        invited = name.rstrip()
-        invited_person = letter_body.replace('[name]', invited)
-        with open(rf"100-Days-of-Code-Projects\Day24\mail_merge\Output\ReadyToSend\{invited}.txt", mode="w") as final_letter:
-            final_letter.write(invited_person)
+        invited_person = name.rstrip()
+        invitation_letter = letter_body.replace('[name]', invited_person)
+        with open(rf"100-Days-of-Code-Projects\Day24\mail_merge\Output\ReadyToSend\{invited_person}.txt", mode="w") as final_letter:
+            final_letter.write(invitation_letter)
