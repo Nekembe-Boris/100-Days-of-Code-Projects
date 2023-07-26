ffb70d7eddf537f2f8f08a15f54f21206aabef39
nekembe <nekembeb@gmail.com>
2023-07-26 19:18:39 +0100

    creating a new txt file

--- Day25/main.py
+++ Day25/main.py
@@ -8,19 +8,21 @@ screen.addshape(image)
 turtle.shape(image)
 
 data = pandas.read_csv("./50_states.csv")
-
 states = data.state
 
 total_states = len(states)
 score = 0
 guessed_states = []
+missed_states = []
 
 def locate(name, pos_x, pos_y):
+    """This function creates a new turtle, gets the name of the state and sends it to its corresponding x and y cooordinates"""
     point = turtle.Turtle()
     point.hideturtle()
     point.penup()
     point.goto(pos_x, pos_y)
     point.write(arg=f"{name}", move=False, align="center", font=("Courier", 10, "bold"))
+        #alternative to the above line could be --> point.write(state.state.item())
 
 
 end = False
@@ -29,6 +31,7 @@ while end != True:
     answer_state = screen.textinput(f"{score}/{total_states} Correct States", "Enter the name of a U.S. State").title()
     for state in states:
         if answer_state == state:
+            #To avoid double counting of states and scores
             if state not in guessed_states:
                 guessed_states.append(state)
                 score += 1
@@ -38,5 +41,17 @@ while end != True:
             state_name = answer_state
             locate(answer_state, x_coor, y_coor)
 
+    if answer_state == "Exit":
+        for state in states:
+            if state not in guessed_states:
+                missed_states.append(state)
+        end = True
 
-turtle.mainloop()
+learn_dict = {
+    "States": missed_states
+}
+
+data_f = pandas.DataFrame(learn_dict)
+data_f.to_csv("States to learn.txt")
+
+screen.exitonclick()
