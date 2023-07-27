165a291f2375367044d684a6fcd95ab5f1fe3a42
nekembe <nekembeb@gmail.com>
2023-07-27 23:34:43 +0100

    renaming variable

--- Day26/main.py
+++ Day26/main.py
@@ -7,9 +7,9 @@ for (index, row) in phonetic_data.iterrows():
     new_dict = {row.letter : row.code for (index, row) in phonetic_data.iterrows()}
 
 
-name = input("Enter a word: ").upper()
+word = input("Enter a word: ").upper()
 
-coded_word = [new_dict[letter] for letter in name]
+coded_word = [new_dict[letter] for letter in word]
 
 
 print(coded_word)
