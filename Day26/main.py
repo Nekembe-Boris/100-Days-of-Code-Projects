89dc88599012270e4b5b90099402111a11165e02
nekembe <nekembeb@gmail.com>
2023-08-17 23:20:17 +0100

    adding exceptions learned on day 30

--- Day26/main.py
+++ Day26/main.py
@@ -6,10 +6,16 @@ phonetic_data = pandas.read_csv("./nato_phonetic_alphabet.csv")
 for (index, row) in phonetic_data.iterrows():
     new_dict = {row.letter : row.code for (index, row) in phonetic_data.iterrows()}
 
+valid = False
 
-word = input("Enter a word: ").upper()
+while valid != True:
 
-coded_word = [new_dict[letter] for letter in word]
+    word = input("Enter a word: ").upper()
 
-
-print(coded_word)
+    try:
+        coded_word = [new_dict[letter] for letter in word]
+    except KeyError:
+        print("Please enter only letters of the alphabet")
+    else:
+        print(coded_word)
+        valid = True
