33778d2c5dfc074230a2e2dcab0810efa5fd86ee
nekembe <nekembeb@gmail.com>
2023-09-04 23:15:46 +0100

    first commit

---
+++ Day34/main.py
@@ -0,0 +1,20 @@
+from data import question_data
+from question_model import QuestionModel
+from quiz_brain import QuizBrain
+
+question_vault = []
+
+for item in question_data:
+    question_vault.append(
+        QuestionModel(item["question"], item["correct_answer"])
+        )
+
+
+quiz = QuizBrain(question_vault)
+
+while quiz.questions_left() == True:
+    quiz.next_question()
+
+
+print("\nYou've completed the quiz")
+print(f"Your final score was {quiz.score}/{quiz.question_number}")
